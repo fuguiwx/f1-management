d6765c9ac7b13180f5883440b553d2c2670c43ed
fuguiwx <hhhhweixin2369@qq.com>
2026-05-20 11:44:12 +0800

    update

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="page-wrapper">
     <h2 class="page-title">用户管理</h2>
-    <el-button class="btn-add" @click="openAdd">新增用户</el-button>
+    <el-button class="btn-add" @click="openAdd"> 新增用户</el-button>
 
     <el-table :data="list" border width="100%">
       <el-table-column prop="id" label="ID" />
@@ -16,9 +16,7 @@
 
     <el-dialog v-model="visible" title="用户信息">
       <el-form v-model="form" label-width="80px">
-        <el-form-item label="昵称">
-          <el-input v-model="form.nickname" />
-        </el-form-item>
+        <el-form-item label="昵称"><el-input v-model="form.nickname" /></el-form-item>
       </el-form>
       <el-button class="btn-add" @click="save">保存</el-button>
     </el-dialog>
@@ -27,58 +25,23 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { ElMessage } from 'element-plus'
 import { userApi } from '../api'
-
 const list = ref([])
 const form = ref({})
 const visible = ref(false)
 
 onMounted(() => getList())
-
 const getList = async () => {
-  try {
-    const res = await userApi.list()
-    list.value = res.data || []
-  } catch (e) {
-    list.value = []
-  }
+  const res = await userApi.list()
+  list.value = res.data.data
 }
-
-const openAdd = () => {
-  form.value = {}
-  visible.value = true
-}
-
-const openEdit = row => {
-  form.value = { ...row }
-  visible.value = true
-}
-
+const openAdd = () => { form.value = {}; visible.value = true }
+const openEdit = (row) => { form.value = { ...row }; visible.value = true }
 const save = async () => {
-  try {
-    if (form.value.id) {
-      await userApi.update(form.value)
-    } else {
-      await userApi.add(form.value)
-    }
-    visible.value = false
-    ElMessage.success('操作成功')
-    getList()
-  } catch (e) {
-    ElMessage.error('操作失败')
-  }
+  form.value.id ? await userApi.update(form.value) : await userApi.add(form.value)
+  visible.value = false; getList()
 }
-
-const del = async id => {
-  try {
-    await userApi.delete(id)
-    ElMessage.success('删除成功')
-    getList()
-  } catch (e) {
-    ElMessage.error('删除失败')
-  }
-}
+const del = async (id) => { await userApi.delete(id); getList() }
 </script>
 
 <style scoped>
